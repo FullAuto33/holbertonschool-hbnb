210db4f2922d28fd488eef091ecaa43126313874
FullAuto33 <46572123+FullAuto33@users.noreply.github.com>
2025-08-04 08:28:01 +0200

    add function to get cookies in js file

--- Part4/script.js
+++ Part4/script.js
@@ -44,3 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
